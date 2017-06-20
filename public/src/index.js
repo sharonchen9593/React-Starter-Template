34575bd2289f27151c3d3e320fb1ed91a2ff3e5a
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-20 11:15:57 -0700

    connected components

--- public/src/index.js
+++ public/src/index.js
@@ -1,11 +1,14 @@
 // dont need to import React because already imported in index.html
 
+import Compnent1 from './components/component1';
+
 class App extends React.Component {
 	
 	render() {
 		return (
 			<div>
-			Hello from index.js
+				Hello from index.js
+				<Compnent1 />
 			</div>
 	);
 	}
