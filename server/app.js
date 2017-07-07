976e1d8a167b0ad8132105ac3ccb0d7b069bd360
sharonchen9593 <sharonchen9593@gmail.com>
2017-07-06 20:10:26 -0700

    added server in separate folder

--- server/app.js
+++ server/app.js
@@ -3,6 +3,6 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.use('/', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public'));
 
 app.listen(port);
