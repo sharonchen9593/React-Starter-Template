5bf3211c7575731ed21cee5b7f21ddd5d4bf7355
sharonchen9593 <sharonchen9593@gmail.com>
2017-07-06 23:51:18 -0700

    set up routing template

--- public/src/index.js
+++ public/src/index.js
@@ -1,18 +1,6 @@
-// dont need to import React because already imported in index.html
-
-import Compnent1 from './components/component1';
-
-class App extends React.Component {
-	
-	render() {
-		return (
-			<div>
-				Hello from index.js
-				<Compnent1 />
-			</div>
-	);
-	}
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
 ReactDOM.render(<App />, document.querySelector('.container'))
 
--- webpack.config.js
+++ webpack.config.js
@@ -18,5 +18,6 @@ module.exports = {
 	},
 	resolve: {
 		extensions: ['.js']
-	}
+	},
+	devtool: 'inline-source-map'
 };
